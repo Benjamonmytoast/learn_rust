9bcade7b0d2ede2239403c14143c6a1d6fbd0c1b
Benjamin Stephenson <1999bstephenson@gmail.com>
2024-03-10 16:26:30 +0000

    Added Fib function with memo dep.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use memoize::memoize;
+
 fn main() {
-    println!("Hello, world!");
+    let greeting = greet("Ben", true);
+    println!("Greeting is {greeting}");
+    println!("{}", fib(35));
+}
+
+fn greet(name: &str, is_intro: bool) -> String {
+    let intro = "Hi guys";
+    let outro = "Bye then";
+    let prefix = if is_intro { intro } else { outro };
+    return format!("{prefix}, it's {name}")
+}
+#[memoize]
+fn fib(n: u32) -> u64 {
+    match n {
+        0 | 1 => 1,
+        _ => fib(n - 1) + fib(n - 2)
+    }
 }
